eb79063e162c69e4cd7588219f2e2e7ed4ee0827
randallsojm <randallsojm@gmail.com>
2026-09-01 17:55:21 +0800

    Create GUI for Duke

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -5,13 +5,18 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class DialogBox extends HBox {
-
     private Label text;
     private ImageView displayPicture;
-
     public DialogBox(String s, Image i) {
         text = new Label(s);
         displayPicture = new ImageView(i);
+
+        //Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
         this.getChildren().addAll(text, displayPicture);
     }
 }
